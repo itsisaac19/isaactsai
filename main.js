d623847a6c0282eba5efd700023c17b8e5dabfe0
itdesigntk <66639356+itdesigntk@users.noreply.github.com>
2020-11-17 21:21:36 -0600

    You know

--- main.js
+++ main.js
@@ -1,56 +1,35 @@
 function start () {
-    if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
-        document.getElementById("backgroundColor").className = "yel";
-      } else {
-        document.getElementById("backgroundColor").className = "defaultbg";
-      }
-    setTimeout(function() {
-        document.getElementById("graphicDesigner").classList.remove("universalHide")
-        document.getElementById("graphicDesigner").classList.add("universalShow")
-        setTimeout(function() {
-            document.getElementById("and").classList.remove("universalHide")
-            document.getElementById("and").classList.add("universalShow")
-            setTimeout(function() {
-                document.getElementById("webDeveloper").classList.remove("universalHide")
-                document.getElementById("webDeveloper").classList.add("universalShow")
-            }, 500)
-        }, 500)
-    }, 100);
-    setTimeout(function() {
-        document.getElementById("divideMiddleBottom").classList.remove("divideHide")
-        document.getElementById("divideMiddleBottom").classList.add("divideShow")
-    }, 100);
 
-    setTimeout(function() {
-        document.getElementById("isaactsai").innerHTML = "ISAAC TSAI"
-        document.getElementById("isaactsai").classList.add("isaactsaiType")
-        setTimeout(function() {
-            document.getElementById("isaactsai").innerHTML = "ISAAC TSAI"
-            document.getElementById("isaactsai").classList.remove("isaactsaiType")
-        }, 2000);
-    }, 2000);
+  //EVERYTHING ON HOMEPAGE START
+  var img = document.getElementById('outlineRec')
+  var name = document.getElementById('adventures')
 
-    setTimeout(function() {
-        document.getElementById("ellipse").classList.remove("ellipseStart")
-        document.getElementById("ellipse").classList.add("ellipseDelay")
-    }, 2001);
-}
+  var divideLine1 = document.getElementById('lineOne')
+  var divideLine2 = document.getElementById('lineTwo')
+  var subTitle = document.getElementById('portfolioSubTitle')
+  var paraExplain = document.getElementById('portfolioExplain')
 
+  //SLIDE IN ELEMENTS
 
+  setTimeout(function() {
+    img.style.top = '0px'
+    img.style.filter = "opacity(1)"
+  }, 500)
 
-window.onscroll = function() {scrollAnimation();
+  setTimeout(function() {
+    name.style.top = '0px'
+    name.style.filter = "opacity(1)"
+  }, 800)
 
-    scrollCalc();
+  setTimeout(function() {
+    subTitle.style.top = '0px'
+    subTitle.style.filter = "opacity(1)"
+  }, 1300)
 
-};
+  setTimeout(function() {
+    paraExplain.style.top = '0px'
+    paraExplain.style.filter = "opacity(1)"
+  }, 1400)
 
-function scrollAnimation() {
-  if (document.body.scrollTop > 180 || document.documentElement.scrollTop > 180) {
-    document.getElementById("backgroundColor").className = "yel";
-
-  } else {
-    document.getElementById("backgroundColor").className = "defaultbg";
-
-  }
 }
 
